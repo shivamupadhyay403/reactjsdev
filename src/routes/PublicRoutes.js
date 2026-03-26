04221407cfec98a6d614ea71b5b4262d20db82ac
shivamu1818 <shivam.gazaniainfotech.com>
2026-03-27 00:14:05 +0530

    adding login page

--- src/routes/PublicRoutes.js
+++ src/routes/PublicRoutes.js
@@ -1,15 +1,9 @@
 import React from 'react'
 const LoginPage = React.lazy(() => import('../pages/UnAuth/Login'))
-const HomePage = React.lazy(() => import('../pages/UnAuth/HomePage'))
 export const PublicRoutes = [
   {
     key: 'homepage',
     path: '/',
-    Component: HomePage,
-  },
-  {
-    key: 'login',
-    path: '/login',
     Component: LoginPage,
   },
 ]
